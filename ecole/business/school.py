d1d87718ec7103319234664f328eb3cfd820e568
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-07 15:15:43 +0200

    get student by id, get teacher by id

--- ecole/business/school.py
+++ ecole/business/school.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass, field
 from datetime import date
 
 from daos.course_dao import CourseDao
+from daos.student_dao import StudentDao
+from daos.teacher_dao import TeacherDao
 from models.address import Address
 from models.course import Course
 from models.teacher import Teacher
@@ -52,6 +54,16 @@ class School:
     def get_course_by_id(id_course: int):
         course_dao: CourseDao = CourseDao()
         return course_dao.read(id_course)
+
+    @staticmethod
+    def get_student_by_id(id_student: int):
+        student_dao: StudentDao = StudentDao()
+        return student_dao.read(id_student)
+
+    @staticmethod
+    def get_teacher_by_id(id_teacher: int):
+        teacher_dao: TeacherDao = TeacherDao()
+        return teacher_dao.read(id_teacher)
 
     def init_static(self) -> None:
         """Initialisation d'un jeu de test pour l'école."""
