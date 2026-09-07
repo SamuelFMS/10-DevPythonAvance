1a5fb7989be762c23905ff58e104b4f2ac986184
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-07 15:15:20 +0200

    Teacher dao

---
+++ ecole/daos/teacher_dao.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from daos import address_dao
+from daos.address_dao import AddressDao
+from daos.dao import Dao
+from models.teacher import Teacher
+
+
+@dataclass
+class TeacherDao(Dao[Teacher]):
+    def create(self, teacher: Teacher) -> int:
+        return 0
+
+    def read(self, id_teacher: int) -> Optional[Teacher]:
+        """Renvoit le l'étudiant correspondant à l'entité dont l'id est id_teacher
+           (ou None s'il n'a pu être trouvé)"""
+        teacher: Optional[Teacher]
+
+        with Dao.connection.cursor() as cursor:
+            sql = (
+                "SELECT * "
+                "FROM teacher "
+                "JOIN person on person.id_person = teacher.id_person "
+                "WHERE id_teacher = %s"
+            )
+            cursor.execute(sql, (id_teacher))
+            record = cursor.fetchone()
+        if record is not None:
+            address_dao: AddressDao = AddressDao()
+            teacher = Teacher(
+                first_name=record['first_name'],
+                last_name=record['last_name'],
+                age=record['age'],
+                hiring_date=record['hiring_date']
+            )
+            if record['id_address']:
+                teacher.address = address_dao.read(record['id_address'])
+        else:
+            teacher = None
+
+        return teacher
+
+
+    def update(self, teacher: Teacher) -> bool:
+        return True
+
+    def delete(self, teacher: Teacher) -> bool:
+        return True
