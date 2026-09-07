9f20d2e1c14ad6b4770db8e6de7bbd36a26365e6
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-07 15:05:04 +0200

    Address dao

---
+++ ecole/daos/address_dao.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from daos.dao import Dao
+from models.address import Address
+
+
+@dataclass
+class AddressDao(Dao[Address]):
+    def create(self, address: Address) -> int:
+        return 0
+
+    def read(self, id_address: int) -> Optional[Address]:
+        """Renvoit l'addresse' correspondant à l'entité dont l'id est id_address
+           (ou None s'il n'a pu être trouvé)"""
+        address: Optional[Address]
+
+        with Dao.connection.cursor() as cursor:
+            sql = (
+                "SELECT * "
+                "FROM address "
+                "WHERE id_address = %s"
+            )
+            cursor.execute(sql, (id_address))
+            record = cursor.fetchone()
+        if record is not None:
+            address = Address(
+                street=record['street'],
+                city=record['city'],
+                postal_code=record['postal_code'])
+        else:
+            address = None
+
+        return address
+
+
+    def update(self, address: Address) -> bool:
+        return True
+
+    def delete(self, address: Address) -> bool:
+        return True
