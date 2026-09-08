2310a32aff8e99b16ffedaad19a3b93dd900c3fe
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-08 14:59:27 +0200

    init test, tests tous les fichiers de test

---
+++ ecole/tests/__init__.py
@@ -0,0 +1,7 @@
+from tests import address_test, course_test, student_test, teacher_test
+
+if __name__ == '__main__':
+    address_test.tests()
+    course_test.tests()
+    student_test.tests()
+    teacher_test.tests()
