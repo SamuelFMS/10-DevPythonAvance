f0dde2bfebce9ef1746c9b316e17e2e9a1aa33e7
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-08 15:07:56 +0200

    Teacher business

---
+++ ecole/business/teacher_business.py
@@ -0,0 +1,26 @@
+from typing import ClassVar
+
+from daos.teacher_dao import TeacherDao
+from models.teacher import Teacher
+
+
+class TeacherBusiness:
+    teacher_dao: ClassVar[TeacherDao]  = TeacherDao()
+
+    @staticmethod
+    def get_all_teachers(self) -> list:
+        teachers: list = []
+        return teachers
+
+
+    @staticmethod
+    def get_teacher_by_id(id_teacher: int):
+        return TeacherBusiness.teacher_dao.read(id_teacher)
+
+    @staticmethod
+    def add_teacher(teacher: Teacher):
+        return TeacherBusiness.teacher_dao.create(teacher)
+
+    @staticmethod
+    def delete_teacher(teacher: Teacher):
+        return TeacherBusiness.teacher_dao.delete(teacher)
