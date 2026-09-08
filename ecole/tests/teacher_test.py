a12ca9e61cd0dc1e779ed4a0d9e35bf0ab70e183
Samuel Curran <samuel.curran@fms-ea.com>
2026-09-08 15:08:14 +0200

    Teacher test utilise Teacher business

--- ecole/tests/teacher_test.py
+++ ecole/tests/teacher_test.py
@@ -1,24 +1,19 @@
 from datetime import date
-
-from business import school
-from business.school import School
-from daos.teacher_dao import TeacherDao
+from business.teacher_business import TeacherBusiness
 from models.teacher import Teacher
 
 
 def tests():
     print("Executing tests for Teacher")
-    school: School = School()
-    teacher_dao = TeacherDao()
 
     # Creation d'un cours
     teacher = Teacher("FirstNameTest", "LastNameTest", 20, date(2021,5,1))
-    new_teacher_id = teacher_dao.create(teacher)
+    new_teacher_id = TeacherBusiness.add_teacher(teacher)
     assert new_teacher_id != 0
     assert teacher.id == new_teacher_id
 
     #Recuperation du cours dans la base de donnée
-    read_teacher = teacher_dao.read(new_teacher_id)
+    read_teacher = TeacherBusiness.get_teacher_by_id(new_teacher_id)
     assert read_teacher is not None
     assert read_teacher.id == new_teacher_id
     assert read_teacher.first_name == 'FirstNameTest'
@@ -27,7 +22,7 @@ def tests():
     assert read_teacher.hiring_date == date(2021,5,1)
 
     #Suppression du cours que nous venons de créer
-    assert teacher_dao.delete(read_teacher)
+    assert TeacherBusiness.delete_teacher(read_teacher)
 
 
 
